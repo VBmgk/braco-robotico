efe9b399ec0ecce50c4ecb6153c70b96a378aa26
Luis Helder <luislhl@vialink.com.br>
2015-05-21 13:30:41 -0300

    Fazendo umas melhorias na detecção da imagem

--- image_detection/multi_color_multiple_points_track.py
+++ image_detection/multi_color_multiple_points_track.py
@@ -21,18 +21,19 @@ import cv
         H: 0-180, S: 0-255, V: 0-255
     So you can use GIMP to analyze a sample image and find out what colors you want, and then transform to OpenCv scale.
 """
-orange_low = cv.Scalar(3,150,150)
-orange_high = cv.Scalar(13,255,255)
 
-blue_low = cv.Scalar(110,150,150)
-blue_high = cv.Scalar(130,255,255) 
+delta = (5, 50, 50)
+
+orange = cv.Scalar(8,205,205)
+
+blue = cv.Scalar(112,205,205)
 
 colors = {
-        'orange': (orange_low, orange_high),
-        'blue': (blue_low, blue_high)
+        'orange': orange,
+        'blue': blue
     }
 
-def getthresholdedimg(im, color_low_threshold, color_high_threshold):
+def getthresholdedimg(im, color):
     '''this function take RGB image.Then convert it into HSV for easy colour detection and threshold it with the color chosen as white and all other regions as black.Then return that image'''
     imghsv=cv.CreateImage(cv.GetSize(im),8,3)
     cv.CvtColor(im,imghsv,cv.CV_BGR2HSV)                # Convert image from RGB to HSV
@@ -40,6 +41,9 @@ def getthresholdedimg(im, color_low_threshold, color_high_threshold):
     # A little change here. Creates images for green,blue and yellow (or whatever color you like).
     imgcolor=cv.CreateImage(cv.GetSize(im),8,1)
     imgthreshold=cv.CreateImage(cv.GetSize(im),8,1)
+
+    color_low_threshold = (color[0] - delta[0], color[1] - delta[1], color[2] - delta[2])
+    color_high_threshold = (color[0] + delta[0], color[1] + delta[1], color[2] + delta[2])
     
     cv.InRangeS(imghsv,color_low_threshold,color_high_threshold,imgcolor) 
     cv.Add(imgthreshold,imgcolor,imgthreshold)
@@ -51,39 +55,63 @@ def run_from_img(original_image):
             im = cv.LoadImage("image.png")
     """
     centroids = {}
+    thresholded_imgs = {}
+    imgthresh = {}
+    color_image = {}
+    storage = {}
+    contour = {}
 
-    for color_name, color_limits in colors.iteritems():
-        color_image = cv.CloneImage(original_image)
-        cv.Smooth(color_image, color_image, cv.CV_GAUSSIAN, 3, 0)
-        imgyellowthresh=getthresholdedimg(color_image, color_limits[0], color_limits[1])
+    for color in colors.keys():
+        color_image[color] = cv.CloneImage(original_image)
+        cv.Smooth(color_image[color], color_image[color], cv.CV_GAUSSIAN, 3, 0)
 
-        cv.Erode(imgyellowthresh,imgyellowthresh,None,3)
-        cv.Dilate(imgyellowthresh,imgyellowthresh,None,5)
-        storage = cv.CreateMemStorage(0)
-        contour = cv.FindContours(imgyellowthresh, storage, cv.CV_RETR_CCOMP, cv.CV_CHAIN_APPROX_SIMPLE)
+        imgthresh[color]=getthresholdedimg(color_image[color], colors[color])
+        thresholded_imgs[color] = cv.CloneImage(imgthresh[color])
 
-        centroids[color_name] = []
-        while contour:
+        cv.Erode(imgthresh[color],imgthresh[color],None,3)
+        cv.Dilate(imgthresh[color],imgthresh[color],None,10)
+        storage[color] = cv.CreateMemStorage(0)
+        contour[color] = cv.FindContours(imgthresh[color], storage[color], cv.CV_RETR_CCOMP, cv.CV_CHAIN_APPROX_SIMPLE)
+
+        centroids[color] = []
+        while contour[color]:
             # Centroid calculation
-            moment = cv.Moments(contour)
+            moment = cv.Moments(contour[color])
             Cx = int(moment.m10 / moment.m00)
             Cy = int(moment.m01 / moment.m00)
 
-            centroids[color_name].append((Cx, Cy))
+            centroids[color].append((Cx, Cy))
 
-            contour = contour.h_next()
+            contour[color] = contour[color].h_next()
 
-    return centroids
+    return centroids, thresholded_imgs
 
 def run_from_cam():
     """ This fuction keeps reading frames from the CAM and passing them to the function run_from_img, for color detection, and printing the centroids found.
     """
-    capture=cv.CaptureFromCAM(0)
-    while(1):
-        color_image = cv.QueryFrame(capture)
-        # Mirror the image from the cam
-        cv.Flip(color_image,color_image,1)
+    #import time
 
-        centroids = run_from_img(color_image)
+    capture=cv.CaptureFromCAM(1)
+    cv.NamedWindow("Real", 0)
+    for color in colors.keys():
+        cv.NamedWindow(color, 0)
 
-        print centroids
+    #while(1):
+    color_image = cv.QueryFrame(capture)
+    # Mirror the image from the cam
+    cv.Flip(color_image,color_image,1)
+
+    centroids, thresholded_imgs = run_from_img(color_image)
+
+    print centroids
+
+    cv.ShowImage("Real", color_image)
+    for color in colors.keys():
+        cv.ShowImage(color, thresholded_imgs[color])
+
+    if cv.WaitKey(33)==1048603:
+        cv.DestroyWindow("Real")
+        for color in colors.keys():
+            cv.DestroyWindow(color)
+
+        #time.sleep(1)
